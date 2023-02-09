150777a5ad935918ef6a0c7a40d5ea062967e241
Callum Macdonald <git@callum-macdonald.com>
2023-02-09 23:03:42 +0100

    Bugfix, remove `/kinds` from URL.

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,2 +1,2 @@
 export const REPO_PUBLIC_URL =
-	'https://raw.githubusercontent.com/chmac/nostr-kind-registry-data/main/kinds';
+	'https://raw.githubusercontent.com/chmac/nostr-kind-registry-data/main';
