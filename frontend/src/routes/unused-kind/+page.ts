6863af931ee567744b41766ebba5011b6831b6df
Simon <abeluga@posteo.net>
2023-02-13 14:20:21 +0100

    shuffle relays list

--- frontend/src/routes/unused-kind/+page.ts
+++ frontend/src/routes/unused-kind/+page.ts
@@ -9,8 +9,18 @@ function getRandomKind(seenKinds: number[]) {
 	}
 }
 
+function shuffle(array: any[]) {
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * i);
+		const temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+	return array;
+}
+
 export const load = async () => {
-	const relays = await getRelaysList();
+	const relays = shuffle(await getRelaysList());
 	const kinds = await getSortedKindsList();
 	const randomKind = getRandomKind(kinds.map(({ kind }) => kind));
 
