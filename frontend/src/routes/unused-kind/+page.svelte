701619fd91a1effbcbd1e6613b302208b161215a
Simon <abeluga@posteo.net>
2023-02-13 11:32:52 +0100

    refactoring unused-kind

--- frontend/src/routes/unused-kind/+page.svelte
+++ frontend/src/routes/unused-kind/+page.svelte
@@ -4,32 +4,15 @@
 	import type { PageData } from './$types';
 	export let data: PageData;
 
-	function getRandomKind() {
-		const min = 100;
-		const max = 10_000;
-		while (true) {
-			let randomKind = Math.floor(Math.random() * (max - min) + min);
-			if (!data.kinds.find(({ kind }) => kind === randomKind)) return randomKind;
-		}
+	function handleCheckRandomKindClick() {
+		getKindFromRelayPromises(data.randomKind, 10);
 	}
-
-	let randomKindPromise: Promise<number> = new Promise(() => {});
-
-	function handleGetRandomKindClick() {
-		const randomKind = getRandomKind();
-		randomKindPromise = Promise.resolve(randomKind);
-		getKindFromRelayPromises(randomKind, 10);
-	}
-
-	type emap = {
-		[index: string]: Promise<NostrEvent>;
-	};
 
 	const eventPromises: { relay: string; eventPromise: Promise<NostrEvent> }[] = data.relays.map(
 		(r) => {
 			return {
 				relay: r,
-				eventPromise: new Promise(() => {}) as Promise<NostrEvent>
+				eventPromise: Promise.reject('not checked yet') as Promise<NostrEvent>
 			};
 		}
 	);
@@ -49,47 +32,46 @@
 </script>
 
 <div class="max-w-prose">
-	<div>
-		<button
-			class="bg-slate-200 hover:bg-slate-300 rounded border"
-			on:click={handleGetRandomKindClick}
-			><span class="p-5">get random unseen kind</span>
-		</button>
-		<div class="mt-5">
-			{#await randomKindPromise then randomKind}
-				<p>Checking kind: {randomKind}</p>
-
-				<table>
-					<thead>
-						<tr>
-							<th>Relay</th>
-							<th>Event status</th>
-						</tr>
-					</thead>
-					<tbody>
-						{#each eventPromises as { relay, eventPromise }}
-							<tr>
-								<td>{relay}:</td>
-								<td>
-									{#await eventPromise}
-										<span>loading…</span>
-									{:then event}
-										<span style="color: red;">oh no! found event {event.id}</span>
-									{:catch error}
-										{#if error === 'event not found'}
-											<span style="color: green;">event not found!</span>
-										{:else if error === 'timed out'}
-											<span style="color: orange">timed out</span>
-										{:else}
-											<span style="color: yellow">errored</span>
-										{/if}
-									{/await}
-								</td>
-							</tr>
-						{/each}
-					</tbody>
-				</table>
-			{/await}
-		</div>
+	<p>
+		Your random kind: {data.randomKind}
+	</p>
+	<button
+		class="bg-slate-200 hover:bg-slate-300 rounded border"
+		on:click={handleCheckRandomKindClick}
+		><span class="p-5">Check It Now!</span>
+	</button>
+	<div class="mt-5">
+		<table>
+			<thead>
+				<tr>
+					<th>Relay</th>
+					<th>Event status</th>
+				</tr>
+			</thead>
+			<tbody>
+				{#each eventPromises as { relay, eventPromise }}
+					<tr>
+						<td>{relay}:</td>
+						<td>
+							{#await eventPromise}
+								<span>checking…</span>
+							{:then event}
+								<span style="color: red;">oh no! found event {event.id}</span>
+							{:catch error}
+								{#if error === 'not checked yet'}
+									<span>not checked yet</span>
+								{:else if error === 'event not found'}
+									<span style="color: green;">event not found!</span>
+								{:else if error === 'timed out'}
+									<span style="color: orange">timed out</span>
+								{:else}
+									<span style="color: yellow">errored</span>
+								{/if}
+							{/await}
+						</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
 	</div>
 </div>
--- frontend/src/routes/unused-kind/+page.ts
+++ frontend/src/routes/unused-kind/+page.ts
@@ -1,7 +1,18 @@
 import { getRelaysList, getSortedKindsList } from '$lib/apis/crawlerApi';
 
+function getRandomKind(seenKinds: number[]) {
+	const min = 100;
+	const max = 10_000;
+	while (true) {
+		let randomKind = Math.floor(Math.random() * (max - min) + min);
+		if (!seenKinds.find((kind) => kind === randomKind)) return randomKind;
+	}
+}
+
 export const load = async () => {
 	const relays = await getRelaysList();
 	const kinds = await getSortedKindsList();
-	return { relays, kinds };
+	const randomKind = getRandomKind(kinds.map(({ kind }) => kind));
+
+	return { relays, kinds, randomKind };
 };
