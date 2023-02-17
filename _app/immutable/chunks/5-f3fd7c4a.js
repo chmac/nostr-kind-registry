6447e4c161269279e6833be6d004f2a5f3c480d2
Callum Macdonald <git@callum-macdonald.com>
2023-02-17 11:35:06 +0100

    Updates

--- _app/immutable/chunks/5-f3fd7c4a.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./_page-2dd2acc5.js";import{default as t}from"../components/pages/unused-kind/_page.svelte-12c74a93.js";export{t as component,r as universal};
---
+++ _app/immutable/modules/pages/unused-kind/_page.ts-a32e4c62.js
@@ -0,0 +1,1 @@
+import"../../../chunks/crawlerApi-090073bd.js";import{l}from"../../../chunks/_page-1e582e78.js";export{l as load};
