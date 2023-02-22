b10e42d489da6818ffd4e6952219ef5862ca2b40
Callum Macdonald <git@callum-macdonald.com>
2023-02-22 18:49:49 +0100

    Switch to the cloudflare adapter.

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { getKindsPaths } from './utils/getKindsPaths.js';
 
